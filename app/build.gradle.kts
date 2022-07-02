c87eb461e7fe8003b0a5b4014069ef7aba481d10
Zin Min Tun <zinmintuncu@gmail.com>
2022-07-02 17:52:31 +0630

    fix:Setup hilt-dagger.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
     kotlin("kapt")
+    id("kotlin-android")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,15 @@ plugins {
     id("com.android.library") version ("7.1.3") apply (false);
     id("org.jetbrains.kotlin.android") version ("1.5.30") apply (false);
 }
+buildscript {
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+        val navVersion = "2.3.5"
+       // classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
 
 tasks.register("clean",Delete::class){
     delete(rootProject.buildDir)
